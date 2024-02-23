e1ddffc9c3984d9a3f3438b8af6fa432f4d09152
Restu Ahmad Ar Ridho <restuaarridho@gmail.com>
2024-02-23 10:17:49 +0700

    refactor: membuat UUID car langsung pada model (SRP)

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
@@ -3,6 +3,8 @@ package id.ac.ui.cs.advprog.eshop.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 public class Car {
@@ -10,4 +12,8 @@ public class Car {
   private String carName;
   private String carColor;
   private int carQuantity;
+
+  public Car() {
+    this.carId = UUID.randomUUID().toString();
+  }
 }
--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
@@ -6,18 +6,12 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.UUID;
 
 @Repository
 public class CarRepository {
-  static int id = 0;
   private List<Car> carData = new ArrayList<>();
 
   public Car create(Car car) {
-    if (car.getCarId() == null) {
-      UUID uuid = UUID.randomUUID();
-      car.setCarId(uuid.toString());
-    }
     carData.add(car);
     return car;
   }
