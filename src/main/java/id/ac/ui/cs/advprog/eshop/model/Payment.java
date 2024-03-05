6b2350af27ca23cfca318892c556595eb51a602c
Restu Ahmad Ar Ridho <restuaarridho@gmail.com>
2024-03-05 18:05:51 +0700

    [REFACTOR] apply payment test to payment

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,79 +1,63 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
-import java.util.Arrays;
 import java.util.Map;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import lombok.Builder;
 import lombok.Getter;
 
-@Builder
 @Getter
 public class Payment {
   String id;
   Order order;
-  String method;
   Map<String, String> paymentData;
   String status;
+  String method;
 
-  public Payment(String id, Order order, String method, Map<String, String> paymentData) {
+  @Builder
+  public Payment(String id, Order order, Map<String, String> paymentData) {
     this.id = id;
-    if (order == null) {
-      throw new IllegalArgumentException();
-    } else {
-      this.order = order;
-    }
-
-    String[] methodList = { "VOUCHER_CODE", "CASH_ON_DELIVERY" };
-    if (Arrays.stream(methodList).noneMatch(item -> item.equals(method))) {
-      throw new IllegalArgumentException();
-    } else {
-      this.method = method;
-    }
-
-    this.status = "PENDING";
-
-    if (paymentData == null || paymentData.isEmpty()) {
-      throw new IllegalArgumentException();
-    }
-
-    if (method.equals("VOUCHER_CODE") && !paymentData.containsKey("voucherCode")) {
-      throw new IllegalArgumentException();
-    } else if (method.equals("VOUCHER_CODE")) {
-      if (paymentData.get("voucherCode") == null || paymentData.get("voucherCode").isEmpty()
-          || !paymentData.get("voucherCode").startsWith("ESHOP")
-          || numericsInVoucherCounter(paymentData.get("voucherCode")) != 8
-          || paymentData.get("voucherCode").length() != 16) {
-        setStatus("REJECTED");
-      }
-    }
-
-    if (method.equals("CASH_ON_DELIVERY") && !paymentData.containsKey("address")
-        && !paymentData.containsKey("deliveryFee")) {
-      throw new IllegalArgumentException();
-    } else if (method.equals("CASH_ON_DELIVERY")) {
-      if (paymentData.get("address") == null || paymentData.get("address").isEmpty()
-          || paymentData.get("deliveryFee") == null || paymentData.get("deliveryFee").isEmpty()) {
-        setStatus("REJECTED");
-      }
-    }
-
-    this.paymentData = paymentData;
+    setOrder(order);
+    setStatus(PaymentStatus.PENDING.getValue());
+    setPaymentData(paymentData);
   }
 
-  public Payment(String id, Order order, String method, Map<String, String> paymentData, String status) {
-    this(id, order, method, paymentData);
+  @Builder
+  public Payment(String id, Order order, Map<String, String> paymentData, String status) {
+    this(id, order, paymentData);
     setStatus(status);
   }
 
+  protected void setOrder(Order order) {
+    if (order == null) {
+      throw new IllegalArgumentException();
+    }
+    this.order = order;
+  }
+
   public void setStatus(String status) {
-    String[] statusList = { "PENDING", "REJECTED", "SUCCESS" };
-    if (Arrays.stream(statusList).noneMatch(item -> item.equals(status))) {
+    if (!PaymentStatus.contains(status)) {
       throw new IllegalArgumentException();
     }
     this.status = status;
   }
 
-  private long numericsInVoucherCounter(String voucherCode) {
+  protected void setMethod(String method) {
+    if (!PaymentMethod.contains(method)) {
+      throw new IllegalArgumentException();
+    }
+    this.method = method;
+  }
+
+  protected void setPaymentData(Map<String, String> paymentData) {
+    if (paymentData == null || paymentData.isEmpty()) {
+      throw new IllegalArgumentException();
+    }
+    this.paymentData = paymentData;
+  }
+
+  protected long numericsInVoucherCounter(String voucherCode) {
     long numberOfNumerics = 0;
 
     for (char c : voucherCode.toCharArray()) {
